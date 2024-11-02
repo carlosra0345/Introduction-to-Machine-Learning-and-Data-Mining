613ba10e2584680a551a836c9fd16f1fbaa1b126
ybennani <114231319+Y-bennani@users.noreply.github.com>
2024-11-02 14:44:13 +0100

    shuffle

--- project2/dataset.py
+++ project2/dataset.py
@@ -2,6 +2,7 @@ from ucimlrepo import fetch_ucirepo
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from sklearn.utils import shuffle  # Import shuffle function
 
 # fetch dataset 
 glass_identification = fetch_ucirepo(id=42) 
@@ -11,4 +12,4 @@ X = glass_identification.data.features
 targets = np.concatenate(glass_identification.data.targets.to_numpy())
 N, M = X.shape
 
-#SHUFFLE EVERYTHING
+X, targets = shuffle(X, targets, random_state=42)
